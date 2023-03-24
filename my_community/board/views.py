203ca0b1b0e728d0ac2f8ffd8c3e2c806f899755
bok-h22 <lmln828@naver.com>
2023-03-24 12:56:31 +0900

    20230324_12

--- my_community/board/views.py
+++ my_community/board/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import BoardForm
+from user.models import User
 
 # Create your views here.
 def board_list(request):
@@ -7,3 +9,22 @@ def board_list(request):
     boards = Board.objects.all().order_by('-id')
     
     return render(request, 'board_list.html', {'boards': boards})
+
+
+def board_write(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_pk = request.session.get('user')
+            user = User.objects.get(pk= user_pk)
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = user
+
+            board.save()
+            return redirect("/board/list/")
+    else:
+        form = BoardForm()
+    return render(request, 'board_write.html', {'form': form})
